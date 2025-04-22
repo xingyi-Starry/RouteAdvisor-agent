c6be0dc01919b8c857d2ddce5d068e871b53f518
xingyi <973294773@qq.com>
2025-04-22 19:31:13 +0800

    cors

--- router/router.go
+++ router/router.go
@@ -4,10 +4,12 @@ import (
 	"route-advisor-agent/services"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func InitRouter(app *fiber.App, c *services.Controller) {
 
 	app.Group("/api/agent").
+		Use(cors.New()).
 		Post("/diary_writer", c.DiaryWriter.DiaryWriterHandler)
 }
